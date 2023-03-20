8ba4f67232936ada62ecfd6c801205be792d3ee0
Kirill Velovatiy <velovatiykirill@gmail.com>
2023-03-20 17:55:16 +0300

    Get methods for work with date and time

--- app/src/main/java/com/velov/olegmobile/httputils/calendar/CalendarUtils.java
+++ app/src/main/java/com/velov/olegmobile/httputils/calendar/CalendarUtils.java
@@ -3,6 +3,12 @@ package com.velov.olegmobile.httputils.calendar;
 import com.velov.olegmobile.httputils.HttpUtils;
 
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -15,6 +21,19 @@ public class CalendarUtils extends HttpUtils {
      */
     public static String CALENDAR_URL = "https://olegbackend.ru/api_booking/event/calendar?cal_date=2022-05-26"; //Динамически вставлять дату
     //YYYY-MM-DD
+
+    //Methods to get current data for query parameters
+    public static String getCurrentDate() {
+        Date now = new Date();
+        return getISO8601StringForDate(now);
+    }
+
+    private static String getISO8601StringForDate(Date date) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
+        dateFormat.setTimeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
+        return dateFormat.format(date);
+    }
+    //===============================================
 
     /**
      * Method for building GET request for calendar data
